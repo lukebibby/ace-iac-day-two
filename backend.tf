86c8f18f6d8caa4d09109c57ee7678b3c38fe1b1
Luke Bibby <luke.bibby@gmail.com>
2022-02-25 08:59:51 +1100

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   required_version = ">= 1.0.2"
   backend "remote" {
-    organization = luminaria
+    organization = "luminaria"
     workspaces {
       name = "ace-iac-day-two"
     }
